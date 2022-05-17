18b6a166f955a71cfe9481a4cc3e191851bd93fa
Vaibhav Matere <vpm00718897@gmail.com>
2022-05-17 16:54:36 +0530

    style : css component library added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        TOY STORE HOMEPAGE
+      CAR STORE HOMEPAGE
       </header>
     </div>
   );
