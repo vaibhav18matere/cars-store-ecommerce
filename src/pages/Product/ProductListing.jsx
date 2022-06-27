79d028bd9ba2fc90e66b9c68bbf7b8667bf0e091
Vaibhav Matere <vpm00718897@gmail.com>
2022-06-27 17:13:03 +0530

    refactor : anchor tag removed

--- src/pages/Product/ProductListing.jsx
+++ src/pages/Product/ProductListing.jsx
@@ -7,7 +7,7 @@ const ProductListing = () => {
         <ul class="navbar nav-fixed d-flex">
           <div class="d-flex">
             <i class="fa fa-car hamberg-btn" aria-hidden="true"></i>
-            <a href="/index.html">
+            <a href="/">
               <p class="nav-title">carGhar - E-commerce app </p>
             </a>
           </div>
@@ -22,17 +22,17 @@ const ProductListing = () => {
           </div>
           <ul class="navbar-right d-end">
             <li>
-              <a href="/cart/cart.html">
+              <a href="/">
                 <i class="fa fa-cart-plus fa-2x" aria-hidden="true"></i>
               </a>
             </li>
             <li>
-              <a href="/wishlist/wishlist.html">
+              <a href="/">
                 <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i>
               </a>
             </li>
             <li>
-              <a href="/user profile/user_profile.html">
+              <a href="/">
                 <img
                   src="https://picsum.photos/id/111/200/300"
                   alt="navbar-img"
@@ -139,7 +139,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -159,7 +159,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -179,7 +179,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -199,7 +199,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -219,7 +219,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -239,7 +239,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -259,7 +259,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -279,7 +279,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -299,7 +299,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
@@ -319,7 +319,7 @@ const ProductListing = () => {
             <div class="car-card">
               <div class="card_img">
                 <figure>
-                  <a href="/product/product.html">
+                  <a href="/">
                     <img src="https://picsum.photos/id/111/200/300" alt="" />
                   </a>
                 </figure>
