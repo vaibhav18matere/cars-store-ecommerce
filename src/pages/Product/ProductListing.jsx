c34ea33782d6b828b97ae82459822b1554e9daf6
Vaibhav Matere <vpm00718897@gmail.com>
2022-06-29 22:45:24 +0530

    refactor : move sidebar to seperate component

--- src/pages/Product/ProductListing.jsx
+++ src/pages/Product/ProductListing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProductListing.css";
 import { products } from "./../../backend/db/products";
 import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 import { Link } from "react-router-dom";
 
 const ProductListing = () => {
@@ -9,127 +10,7 @@ const ProductListing = () => {
     <>
       <Navbar />
       <section className="middleSection">
-        <div className="sidebar">
-          <div className="sidebar-content">
-            <p>Filters</p>
-            <p>Clear</p>
-          </div>
-          <div className="prize-range">
-            <div className="range">
-              <p>$5K</p>
-              <p>$10K</p>
-              <p>$15K</p>
-            </div>
-            <input
-              className="input-progress"
-              type="range"
-              min="1"
-              max="100"
-              value="50"
-            />
-          </div>
-          <div className="sidebar-sub-content">
-            <div className="filtering-option">
-              <h2 className="sidebar-category text-center">Cateogry</h2>
-              <label className="select-input">
-                <input
-                  type="checkbox"
-                  name="light"
-                  className="checkbox-input"
-                  value=""
-                />
-                <span>SUV's</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="checkbox"
-                  name="light"
-                  className="checkbox-input"
-                  value=""
-                />
-                <span>Super Sports</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="checkbox"
-                  name="light"
-                  className="checkbox-input"
-                  value=""
-                />
-                <span>Trucks</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="checkbox"
-                  name="light"
-                  className="checkbox-input"
-                  value=""
-                />
-                <span>Electric and Hybrid</span>
-              </label>
-            </div>
-            <div className="filtering-option">
-              <h2 className="text-center m-5">Discount Range</h2>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>50% and above</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>40% and above</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>30% and above</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>20% and above</span>
-              </label>
-            </div>
-            <div className="filtering-option ">
-              <h2 className="text-center">Sorted by</h2>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>Price - High to Low</span>
-              </label>
-              <label className="select-input">
-                <input
-                  type="radio"
-                  name="light"
-                  className="radio-input"
-                  value=""
-                />
-                <span>Price - Low to High</span>
-              </label>
-            </div>
-          </div>
-        </div>
+        <Sidebar />
         <div className="content-box">
           <div className="product-container d-flex">
             {products.map(({ _id, title, price, image }) => (
